dc443fe664e0f068f83c089ef15bcc400f26995f
sorenxing <sorenxing@outlook.com>
2018-02-25 15:00:59 +0800

    Init web-console and test project

---
+++ pangolincrawler-web-console/src/main/java/org/pangolincrawler/web/console/App.java
@@ -0,0 +1,13 @@
+package org.pangolincrawler.web.console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
